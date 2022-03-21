1898045d8545842c5b15d42fcf09e0d3213d08bd
Michel Bolzon Souza dos Reis <michel.reis@>
2022-03-21 19:19:07 -0300

    Implementado ATM

--- atm/index.php
+++ atm/index.php
@@ -1,9 +1,28 @@
 <?php
 function atm($withdrawAmount): string
 {
-    $
+    $types = [
+        100,
+        50,
+        20,
+        10,
+        5,
+        1
+    ];
+    if(is_float($withdrawAmount)){
+        return 'Este valor não é válido';
+    }
     if($withdrawAmount === 0){
         return 'Este valor não é válido';
     }
-    return "100: ";
+    $return = [];
+    foreach ($types as $type){
+        $return[$type] = intval($withdrawAmount/$type);
+        $withdrawAmount = $withdrawAmount - ($return[$type] * $type);
+    }
+    return formatReturn($return);
+}
+
+function formatReturn($return){
+    return "[100 => $return[100], 50 => $return[50], 20 => $return[20], 10 => $return[10], 5 => $return[5], 1 => $return[1]]";
 }
--- tests/Atm/AtmTest.php
+++ tests/Atm/AtmTest.php
@@ -7,21 +7,65 @@ use PHPUnit\Framework\TestCase;
 class AtmTest extends TestCase
 {
 
-    public function testCountABigger()
+    public function testMestreDosCodigosExample()
     {
-//        valor = 289
-//retorno = [100 => 2, 50 => 1, 20 => 1, 10 => 1, 5 => 1, 1 => 4]
-        /** Palavra maior */
+        include_once('./atm/index.php');
         $this->assertEquals(
-            "Existem 1 letra 'a' na string ca.",
-            countA('cachorro', 2)
+            "[100 => 2, 50 => 1, 20 => 1, 10 => 1, 5 => 1, 1 => 4]",
+            atm(289)
         );
     }
 
     public function testVazio()
     {
-//        valor = 0
-//retorno = Este valor não é válido
+        include_once('./atm/index.php');
+        $this->assertEquals(
+            "Este valor não é válido",
+            atm(0)
+        );
+    }
+
+    public function testFloat()
+    {
+        include_once('./atm/index.php');
+        $this->assertEquals(
+            "Este valor não é válido",
+            atm(1.5)
+        );
+    }
+
+    public function testBasicCount()
+    {
+        include_once('./atm/index.php');
+        $this->assertEquals(
+            "[100 => 1, 50 => 0, 20 => 0, 10 => 0, 5 => 0, 1 => 0]",
+            atm(100)
+        );
+
+        $this->assertEquals(
+            "[100 => 0, 50 => 1, 20 => 0, 10 => 0, 5 => 0, 1 => 0]",
+            atm(50)
+        );
+
+        $this->assertEquals(
+            "[100 => 0, 50 => 0, 20 => 1, 10 => 0, 5 => 0, 1 => 0]",
+            atm(20)
+        );
+
+        $this->assertEquals(
+            "[100 => 0, 50 => 0, 20 => 0, 10 => 1, 5 => 0, 1 => 0]",
+            atm(10)
+        );
+
+        $this->assertEquals(
+            "[100 => 0, 50 => 0, 20 => 0, 10 => 0, 5 => 1, 1 => 0]",
+            atm(5)
+        );
+
+        $this->assertEquals(
+            "[100 => 0, 50 => 0, 20 => 0, 10 => 0, 5 => 0, 1 => 1]",
+            atm(1)
+        );
     }
 
 }
